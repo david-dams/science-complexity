20dd116021847ee4576ffa012b16c1022c11d824
r2dd <david.dams@tutanota.com>
2025-08-19 09:51:40 +0200

    fix, refactor: node count, statistics

--- process.py
+++ process.py
@@ -14,6 +14,11 @@ import pandas as pd
 
 import subprocess
 
+import statsmodels.api as sm
+from statsmodels.tsa.stattools import adfuller, kpss
+
+from pprint import pprint
+
 ### FILE HANDLING
 def save(res, name = 'data'):
     with open(f'{name}.json', 'w') as fp:
@@ -87,13 +92,41 @@ def tex_to_xml():
     # Print the output
     print("STDOUT:", result.stdout)
     print("STDERR:", result.stderr)
-
+    
 def compute_depth(elem, current=0):
     """Compute max depth of XML tree."""
+    
+    # only increment for legal
     children = list(elem)
     if not children:
         return current
+    
     return max(compute_depth(child, current + 1) for child in children)
+
+def is_token(elem):
+    return 'role' in elem.keys()
+
+def print_elem(elem):
+    print(len(list(elem)))
+    for x in list(elem):
+        print(" ", x.text, " ")
+        
+def print_tree(elem, current = 0, show_level = False):
+    
+    if is_token(elem):
+        level = f"level: {current}" if show_level else ""
+        print(" " * current, elem.text or dict(elem.items())["role"], level)
+
+    children = list(elem)
+    for child in children:
+        print_tree(child, current + 1)        
+        
+def compute_nodes(elem, current=0):
+    """Compute number of nodes in XML tree."""
+    children = list(elem)
+    if not children:
+        return current + is_token(elem)
+    return sum(compute_nodes(child, current + is_token(elem)) for child in children)
 
 def process_equation(eq, label):
     # Define namespace mapping
@@ -104,16 +137,12 @@ def process_equation(eq, label):
     year = parse_time_to_centuries(labels[0].split("=")[-1])
     country = labels[1].split("=")[-1]
     name = labels[2].split("=")[-1]        
-
     xmath = eq.find('.//ltx:XMath', namespaces=ns)
-    depth = None
-    if xmath is not None:
-        depth = compute_depth(xmath)
-
+    
     return {"year" : year,
             "name" : name,
             "country" : country,
-            "depth" : depth}
+            "raw" : xmath}
     
 def xml_to_pandas(data):    
     tree = etree.parse("data.xml")
@@ -132,8 +161,8 @@ def xml_to_pandas(data):
     group_data = []
     for group in equation_groups:
         labels = group.get('labels')
-        dict_list += [process_equation(eq, labels) for eq in group]
-
+        dict_list += [process_equation(eq, labels) for eq in group.findall(".//ltx:equation", namespaces=ns)]
+        
     df = pd.DataFrame(dict_list)
     
     return df
@@ -174,24 +203,6 @@ def plot_places(df):
     plt.tight_layout()  # Adjust layout to fit all elements nicely
     plt.savefig('birth_places.pdf')
 
-def plot_depth(df):
-    # Plotting
-    plt.figure(figsize=(10, 6))  # Set figure size
-    plt.plot(df.year, df.depth, 'o', color='blue', alpha=0.7, label='Complexity')  # Add color and transparency
-
-    # Enhancing the plot
-    plt.title('Expression Complexity Over Time', fontsize=16, weight='bold')  # Improve title styling
-    plt.xlabel('Year', fontsize=14)  # Label for the x-axis
-    plt.ylabel('Complexity', fontsize=14)  # Label for the y-axis
-    plt.xticks(fontsize=12)  # Adjust x-axis tick font size
-    plt.yticks(fontsize=12)  # Adjust y-axis tick font size
-    plt.grid(True, linestyle='--', alpha=0.6)  # Add gridlines for clarity
-    plt.legend(fontsize=12, loc='best')  # Add a legend
-
-    # Highlighting trends
-    plt.tight_layout()  # Adjust layout to prevent clipping
-    plt.savefig('complexity_time.pdf')
-    
 def plot_years(df):
     # Plotting
     plt.figure(figsize=(10, 6))  # Set figure size
@@ -209,28 +220,29 @@ def plot_years(df):
     plt.tight_layout()  # Ensure all elements fit nicely
     plt.savefig('years_distribution.pdf')
 
-def plot_depth_hist(df):    
+def plot_hist(df, key, bins = None, labels = None, label = None):    
     # Reversing the order of bins
-    bins = [-np.inf, 15, 18, np.inf]
-    labels = ['Before 15', '15-18', 'After 18']
+    bins = bins or [-np.inf, 15, 18, np.inf]
+    labels = labels or ['Before 15', '15-18', 'After 18']
 
     # Assigning reversed categories to the DataFrame
     df['time_period'] = pd.cut(df['year'], bins=bins, labels=labels)
 
     # outliers
-    df = df[df['depth'] < 100]
+    df = df[df[key] < 100]
 
     # Plotting
     plt.figure(figsize=(10, 6))  # Set figure size
     for period in labels[::-1]:
-        df[df['time_period'] == period]['depth'].hist(
+        df[df['time_period'] == period][key].hist(
             bins='auto', alpha=0.5, label=period, edgecolor='black'
         )
 
     # Enhancing the plot
-    plt.title('Complexity Distribution for Different Time Periods', fontsize=16, weight='bold')
-    plt.xlabel('Complexity', fontsize=14)
-    plt.ylabel('Frequency', fontsize=14)
+    label = label or 'Complexity'
+    plt.title(f'{label} Distribution for Different Time Periods', fontsize=16, weight='bold')
+    plt.xlabel(f"{label}", fontsize=14)
+    plt.ylabel('Count', fontsize=14)
     plt.legend(fontsize=12, title='Time Period')
     plt.xticks(fontsize=12)
     plt.yticks(fontsize=12)
@@ -238,16 +250,111 @@ def plot_depth_hist(df):
 
     # Final adjustments
     plt.tight_layout()
-    plt.savefig('complexity_hist.pdf')
-           
-if __name__ == '__main__':
+    plt.savefig(f'{key}_hist.pdf')
+    
+def plot_complexity(df, key, year = -np.inf, label = None):
+    # Plotting
+    plt.figure(figsize=(10, 6))  # Set figure size
+    # plt.plot(df.year, df.complexity, 'o', color='blue', alpha=0.7, label='Complexity')  # Add color and transparency
+    
+    # Group by year and calculate mean & standard deviation
+    summary = df.groupby('year')[key].agg(['mean', 'std']).reset_index()
+
+    # Plot
+    label = label or 'Complexity'
+    plt.figure(figsize=(8, 5))
+    idxs = summary["year"] >= year
+    plt.errorbar(summary['year'][idxs], summary['mean'][idxs], yerr=summary['std'][idxs],
+                 fmt='o-', color='blue', ecolor='gray', capsize=5, alpha=0.8,
+                 label=f'Mean {label} ± SD')
+    
+    # Enhancing the plot
+    plt.title(f'Expression {label} Over Time', fontsize=16, weight='bold')  # Improve title styling
+    plt.xlabel('Year', fontsize=14)  # Label for the x-axis
+    plt.ylabel(label, fontsize=14)  # Label for the y-axis
+    plt.xticks(fontsize=12)  # Adjust x-axis tick font size
+    plt.yticks(fontsize=12)  # Adjust y-axis tick font size
+    plt.grid(True, linestyle='--', alpha=0.6)  # Add gridlines for clarity
+    plt.legend(fontsize=12, loc='best')  # Add a legend
+
+    # Highlighting trends
+    plt.tight_layout()  # Adjust layout to prevent clipping
+    plt.savefig(f'{key}_time.pdf')
+    
+def get_statistics(df, key):
+    X = df[key]
+    legal = ~np.logical_or(df.year.isna(), df[key].isna())
+    years = np.array(df.year[legal])
+    complexity = np.array(df[key][legal])
+
+    # OLS regression
+    X_with_const = sm.add_constant(years)
+    model = sm.OLS(complexity, X_with_const).fit()
+
+    adf_result = adfuller(complexity)
+    kpss_result = kpss(complexity, regression='c', nlags='auto')
+    complexity_diff = np.diff(complexity)
+    kpss_diff_result = kpss(complexity_diff, regression='c', nlags='auto')
+
+    return {
+        "OLS": {
+            "slope": model.params[1],
+            "p_value": model.pvalues[1],
+        },
+        "ADF": {
+            "statistic": adf_result[0],
+            "p_value": adf_result[1],
+            "critical_values": adf_result[4],
+        },
+        "KPSS": {
+            "statistic": kpss_result[0],
+            "p_value": kpss_result[1],
+            "critical_values": kpss_result[3],
+        },
+        "KPSS_diff": {
+            "statistic": kpss_diff_result[0],
+            "p_value": kpss_diff_result[1],
+            "critical_values": kpss_diff_result[3],
+        },
+    }
+
+def get_df_from_raw_data():
+    raparse()
+    return get_gf()
+    
+def reparse():
+    data_to_tex("raw.json")
+    tex_to_xml()
+
+def get_df():
     DATA_FILE = 'raw'
     equations = extract_data(DATA_FILE)
-    # data_to_tex(equations)
-    # tex_to_xml()
     df = xml_to_pandas(equations)
+    return df
 
-    plot_places(df)
-    plot_depth(df)
-    plot_years(df)
-    plot_depth_hist(df)
+def debug_info():
+    eq = df.iloc[0].raw
+    print_tree(eq); print(compute_nodes(eq)); print(compute_depth(eq))
+    eq = df.iloc[-1].raw
+    print_tree(eq); print(compute_nodes(eq)); print(compute_depth(eq))
+
+def attach_column(df, func, label):
+    col = len(df.columns)
+    df.insert(col, label, [func(x) if x is not None else None for x in df.raw ])
+    
+if __name__ == '__main__':
+    df = get_df()
+
+    attach_column(df, compute_depth, "depth")
+    attach_column(df, compute_nodes, "nodes")
+        
+    # plot_places(df)
+    # plot_years(df)
+    
+    plot_complexity(df, year = 15, key = "depth", label = "Tree Depth")
+    plot_hist(df, key = "depth", label = "Tree Depth")
+    pprint(get_statistics(df, "depth"))
+
+    plot_complexity(df, year = 15, key = "nodes", label = "Node Count")
+    plot_hist(df, key = "nodes", label = "Node Count")
+    pprint(get_statistics(df, "nodes"))
